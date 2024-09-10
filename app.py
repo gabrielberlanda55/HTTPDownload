cb6959e35319a8d15a7f6367a5eb2c3ae022f256
gabrielberlanda55 <gabrielsberlanda@gmail.com>
2024-09-10 18:11:35 -0300

    Alterado useragent

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def download_music_from_youtube(youtube_url):
 
     # Configurações para baixar o áudio em formato MP3
     ydl_opts = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
         'format': 'bestaudio/best',
         'outtmpl': f'{temp_file_path}.%(ext)s',  # yt-dlp adiciona a extensão correta
         'sleep_interval': 10,  
